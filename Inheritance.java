318b05380a2204a856136ed946b2426c91eab055
Gugu-Blip2002 <jagannathsahoo8658@email>
2024-09-09 16:27:10 +0530

    Add New Programme

---
+++ Inheritance.java
@@ -0,0 +1,14 @@
+package OOPSRepo;
+
+public class Inheritance {
+    String name ;
+    Float Price;
+
+    void study() {
+        System.out.println(name + " is studying on Frame boot...");
+    }
+
+    void buy() {
+        System.out.println(Price + " of tomatoes per kilo...");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+package OOPSRepo;
+
+public class Main {
+    public static void main(String[] args) {
+        Inheritance I1 = new Inheritance();
+        I1.name = "Jagannath";
+        I1.Price = 19.23F;
+        
+        I1.study();
+        I1.buy();
+
+    }
+}
